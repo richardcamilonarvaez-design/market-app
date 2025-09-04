542b8fe3cf807343501e711989838931c295aa54
Richard N <rcnarvaez.0906@unicesmag.edu.co>
2025-09-04 11:12:39 -0500

    Data_base update

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+    //Database connection
+    $host ="localhost"; //127.0.0.1
+    $user ="postgres";
+    $password= "unicesmag";
+    $dbname="marketapp";
+    $port= "5432"; //puerto por defecto de postgres
+
+    $data_connection= "
+        host=$host
+        user=$user
+        password=$password
+        dbname=$dbname
+        port=$port
+    ";
+
+    $conn=pg_connect($data_connection);
+
+    if(!$conn){
+        echo"Error";
+    }else{
+        echo"Connection successfully :::";
+    }
+?>
