c11fd08414ed5e5f20508447ad99dfe0d5f106c0
Richard N <rcnarvaez.0906@unicesmag.edu.co>
2025-09-04 11:23:43 -0500

    Data_base update2

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@
     $conn=pg_connect($data_connection);
 
     if(!$conn){
-        echo"Error";
+        echo"Error ".pg_last_error();
     }else{
         echo"Connection successfully :::";
     }
