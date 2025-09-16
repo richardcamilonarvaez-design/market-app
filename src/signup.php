4f3ed8a09d6e2fd3e578d78b680486089bfd5efb
Richard N <rcnarvaez.0906@unicesmag.edu.co>
2025-09-16 15:33:50 -0500

    Sign.in and Sign.up update

--- src/signup.php
+++ src/signup.php
@@ -12,32 +12,48 @@
 
     $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
 
-    //Step 3: create query to insert into
-    $query="
-        INSERT INTO users (
-            firstname,
-            lastname,
-            mobile_number, 
-            id_number,
-            email,
-            password
-        ) VALUES (
-            '$f_name', 
-            '$l_name', 
-            '$m_number', 
-            '$id_number', 
-            '$e_mail', 
-            '$enc_pass'
-        )
+    $check_email= "
+        SELECT
+            u.email
+        FROM
+            users.u
+        WHERE
+            email='$e_mail' or id_number='$id_number'
+            LIMIT 1
     ";
+    $res_check=pg_query($conn, $check_email);
+    if(pg_num_rows($res_check)>0){
+        echo "<sript>alert('Succes !!! Go to login')</script";
+        header('refresh:0; url=signup.html');
+    }else{
+        //Step 3: create query to insert into
+        $query="
+            INSERT INTO users (
+                firstname,
+                lastname,
+                mobile_number, 
+                id_number,
+                email,
+                password
+            ) VALUES (
+                '$f_name', 
+                '$l_name', 
+                '$m_number', 
+                '$id_number', 
+                '$e_mail', 
+                '$enc_pass'
+            )
+        ";
 
-     //Step 4: Execute query
-     $res=pg_query($conn, $query);
+        //Step 4: Execute query
+        $res=pg_query($conn, $query);
 
-     //Step 5: Validate result
-     if($res){
-        echo "User has been created succesfully !!!";
-     }else{
-        echo "Something wrong";
-     }
+        //Step 5: Validate result
+        if($res){
+            echo "<sript>alert('Succes !!! Go to login')</script";
+            header('refresh:0; url=signin.html');
+        }else{
+            echo "Something wrong";
+        }
+    }
     ?>
